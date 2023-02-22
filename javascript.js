f0a24aa86caaf7a0d7f9c6ecb0c22912732d3f83
Manuel Cervantes <cervantesm2395@gmail.com>
2023-02-22 13:13:50 -0500

    Rock Papers Scissors game logic done

--- javascript.js
+++ javascript.js
@@ -1,25 +1,61 @@
-function computerPlay () { 
+function getComputerChoice() {
+    const words = ['Rock', 'Paper', 'Scissors'];
+    let word = words[Math.floor(Math.random() * words.length)];
+    return word;
+}
 
-    function randomNumber (min, max){
-        min = Math.ceil(1);
-        max = Math.floor(3);
-        return Math.floor(Math.random()*(max-min+1)+min);
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toUpperCase();
+    if (player == "ROCK") {
+        switch (computerSelection) {
+            case 'Rock':
+                return "Draw!";
+            case "Paper":
+                return "You lose! Paper beats Rock";
+            case "Scissors":
+                return "You win! Rock beats Scissors";
+        }
+    }
+    if (player == "SCISSORS") {
+        switch (computerSelection) {
+            case 'Scissors':
+                return "Draw!";
+            case "Paper":
+                return "You win! Scissors beats Paper";
+            case "Rock":
+                return "You lose! Rock beats Scissors";
+        }
+    }
+    if (player == "PAPER") {
+        switch (computerSelection) {
+            case 'Scissors':
+                return "You lose! Scissors beats Paper";
+            case "Paper":
+                return "Draw!";
+            case "Rock":
+                return "You win! Paper beats Rock";
+        }
+    }
+}
+
+function game() {
+    let playerWins = 0;
+    let cpuWins = 0;
+    let draws = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let userPlay = prompt("Rock, Paper or Scissors?");
+        let result = playRound(userPlay, getComputerChoice());
+        if (result.includes('You win')) playerWins++;
+        if (result.includes('You lose')) cpuWins++;
+        if (result.includes('Draw')) draws++;
     }
 
-    let Number = randomNumber();
-
-    if (Number === 1) {
-        let computerPick = 'Rock';
-        console.log(computerPick);
-        
+    if (playerWins == cpuWins) {
+        return "It's a draw!"
+    } else if (playerWins > cpuWins) {
+        return "You won!"
+    } else {
+        return "Cpu won!"
     }
-    else if (Number === 2) {
-        let computerPick = 'Paper';
-        console.log(computerPick);       
-    }
-    else {
-        let computerPick = 'Scissors';
-        console.log(computerPick);
-    }
-    
 }
