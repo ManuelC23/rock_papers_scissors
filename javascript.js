1675348adaaa0551f03f8cd032780e61a7a719a0
Manuel Cervantes <cervantesm2395@gmail.com>
2023-02-22 16:08:19 -0500

    UI and Code Update

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let playerWins = 0;
+let cpuWins = 0;
+let draws = 0;
+
 function getComputerChoice() {
     const words = ['Rock', 'Paper', 'Scissors'];
     let word = words[Math.floor(Math.random() * words.length)];
@@ -11,9 +15,9 @@ function playRound(playerSelection, computerSelection) {
             case 'Rock':
                 return "Draw!";
             case "Paper":
-                return "You lose! Paper beats Rock";
+                return "You lose! CPU picked Paper";
             case "Scissors":
-                return "You win! Rock beats Scissors";
+                return "You win! CPU picked Scissors";
         }
     }
     if (player == "SCISSORS") {
@@ -21,41 +25,65 @@ function playRound(playerSelection, computerSelection) {
             case 'Scissors':
                 return "Draw!";
             case "Paper":
-                return "You win! Scissors beats Paper";
+                return "You win! CPU picked Paper";
             case "Rock":
-                return "You lose! Rock beats Scissors";
+                return "You lose! CPU picked Rock";
         }
     }
     if (player == "PAPER") {
         switch (computerSelection) {
             case 'Scissors':
-                return "You lose! Scissors beats Paper";
+                return "You lose! CPU picked Scissors";
             case "Paper":
                 return "Draw!";
             case "Rock":
-                return "You win! Paper beats Rock";
+                return "You win! CPU picked Rock";
         }
     }
 }
 
-function game() {
-    let playerWins = 0;
-    let cpuWins = 0;
-    let draws = 0;
-
-    for (let i = 0; i < 5; i++) {
-        let userPlay = prompt("Rock, Paper or Scissors?");
-        let result = playRound(userPlay, getComputerChoice());
-        if (result.includes('You win')) playerWins++;
-        if (result.includes('You lose')) cpuWins++;
-        if (result.includes('Draw')) draws++;
+function game(event) {
+    roundWinner.innerText = playRound(event.target.id, getComputerChoice())
+    if (roundWinner.innerText.includes('You win')) playerWins++;
+    if (roundWinner.innerText.includes('You lose')) cpuWins++;
+    if (roundWinner.innerText.includes('Draw')) draws++;
+    if (cpuWins === 5 || playerWins === 5) {
+        if (cpuWins == 5) showAlert("The CPU won the game");
+        if (playerWins == 5) showAlert("You won the game!");
+        playerWins = 0;
+        cpuWins = 0;
+        draws = 0;
     }
-
-    if (playerWins == cpuWins) {
-        return "It's a draw!"
-    } else if (playerWins > cpuWins) {
-        return "You won!"
-    } else {
-        return "Cpu won!"
-    }
+    playerScore.innerText = `Player Score: ${playerWins}`;
+    cpuScore.innerText = `CPU Score: ${cpuWins}`;
+    drawsScore.innerText = `Draws: ${draws}`;
 }
+
+const roundWinner = document.querySelector('.roundWinner');
+const playerScore = document.querySelector('.playerScore');
+const cpuScore = document.querySelector('.cpuScore');
+const drawsScore = document.querySelector('.drawScore');
+
+const rock = document.querySelector("#rock");
+rock.addEventListener('click', (event) => game(event));
+
+const paper = document.querySelector("#paper");
+paper.addEventListener('click', (event) => game(event));
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener('click', (event) => game(event));
+
+function showAlert(text) {
+    const alertText = document.querySelector('.alert-text');
+    alertText.innerText = text + "\nPress Ok to play Again";
+    const alertContainer = document.querySelector('.alert-container');
+    alertContainer.style.display = 'flex';
+}
+
+function hideAlert() {
+    const alertContainer = document.querySelector('.alert-container')
+    alertContainer.style.display = 'none';
+}
+
+const closeAlert = document.querySelector('.alert-button');
+closeAlert.addEventListener('click', hideAlert);
